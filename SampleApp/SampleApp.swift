f5433ba6b6b1e22afa2f705b12f759318509bc1a
Ola <ollepko@gmail.com>
2023-05-28 16:34:43 +0200

    Initial Commit

---
+++ SampleApp/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Aleksandra Łepkowska on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleApp/View/AddItemView.swift
@@ -0,0 +1,54 @@
+//
+//  AddItemView.swift
+//  SampleApp
+//
+//  Created by Aleksandra Łepkowska on 28/05/2023.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var name: String = ""
+    @State private var category: String = "Kids"
+    @State private var deliveryType: String = "Pickup only"
+    
+    let categories = ["Clothes", "Kids", "Home", "Garder",  "Car Parts", "Other"]
+    let deliveryTypes = ["Pickup only", "Shipping only", "Pickup & shipping"]
+    
+    // MARK: - BODY
+    var body: some View {
+        VStack {
+            Form {
+                // MARK: - ITEM NAME
+                TextField("Item name", text: $name)
+                
+                //MARK: - ITEM CATEGORY
+                Picker("Category", selection: $category){
+                    ForEach(categories, id: \.self) {
+                        Text($0)
+                    }
+                }//: ITEM CATEGORY
+                
+                //MARK: - ITEM DELIVERY
+                Picker("Delivery", selection: $deliveryType){
+                    ForEach(deliveryTypes, id: \.self) {
+                        Text($0)
+                    }
+                }//: ITEM DELIVERY
+                .pickerStyle(SegmentedPickerStyle())
+            } //: FORM
+            
+            Spacer()
+        } //: VSTACK
+        .navigationBarTitle("Add free item", displayMode: .inline)
+    } //: NAVIGATION
+}
+
+    // MARK: - PREVIEW
+struct AddItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddItemView()
+    }
+}
